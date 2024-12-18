6372d69b870ad325b5cc1eeb3abda48652be0541
janepanov <panovjane@gmail.com>
2024-12-18 11:28:52 +0100

    Add prop image to ProfileCard component

--- src/App.jsx
+++ src/App.jsx
@@ -7,20 +7,26 @@ import AlexaImage from "./images/alexa.png";
 import CortanaImage from "./images/cortana.png";
 import SiriImage from "./images/siri.png";
 
-console.log(AlexaImage);
-console.log(SiriImage);
-
 function App() {
   return (
     <div>
       <div>Personal Digital Assistants</div>
 
-      <img src={AlexaImage} />
-      <img src={SiriImage} />
-
-      <ProfileCard title="Alexa" handle="@alexa99"></ProfileCard>
-      <ProfileCard title="Cortana" handle="@cortana32"></ProfileCard>
-      <ProfileCard title="Siri" handle="@siri01"></ProfileCard>
+      <ProfileCard
+        title="Alexa"
+        handle="@alexa99"
+        image={AlexaImage}
+      ></ProfileCard>
+      <ProfileCard
+        title="Cortana"
+        handle="@cortana32"
+        image={CortanaImage}
+      ></ProfileCard>
+      <ProfileCard
+        title="Siri"
+        handle="@siri01"
+        image={SiriImage}
+      ></ProfileCard>
     </div>
   );
 }
--- src/ProfileCard.jsx
+++ src/ProfileCard.jsx
@@ -1,10 +1,11 @@
-function ProfileCard({ title, handle }) {
+function ProfileCard({ title, handle, image }) {
   //   const { title, handle } = props;
   //   const title = props.title;
   //   const handle = props.handle;
 
   return (
     <div>
+      <img src={image} alt="PDA Logo"></img>
       <div>Title is {title}</div>
       <div>Handle is {handle}</div>
     </div>
