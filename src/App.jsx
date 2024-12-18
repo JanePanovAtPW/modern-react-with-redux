0e50b2fc10591625b31ac80a452c767647583079
janepanov <panovjane@gmail.com>
2024-12-18 11:07:56 +0100

    Include images in components

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,20 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import ProfileCard from "./ProfileCard";
+import AlexaImage from "./images/alexa.png";
+import CortanaImage from "./images/cortana.png";
+import SiriImage from "./images/siri.png";
+
+console.log(AlexaImage);
+console.log(SiriImage);
 
 function App() {
   return (
     <div>
       <div>Personal Digital Assistants</div>
+
+      <img src={AlexaImage} />
+      <img src={SiriImage} />
 
       <ProfileCard title="Alexa" handle="@alexa99"></ProfileCard>
       <ProfileCard title="Cortana" handle="@cortana32"></ProfileCard>
